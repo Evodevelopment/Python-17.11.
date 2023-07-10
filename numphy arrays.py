c5f03aa2f9acc01d18764d650d6c3e61cf11a67b
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-10 08:20:28 +0200

    Update numphy arrays.py

--- numphy arrays.py
+++ numphy arrays.py
@@ -8,3 +8,17 @@ numpy2 = np.array([13.0, 24.0, 8.25, 9.0])
 output = np.logical_and(numpy1 > 10, numpy2 < 20)
 print(output)
 
+#example1
+import pandas as pd
+
+datacamp = pd.DataFrame({
+    'course': ['python', 'r', 'sql'],
+    'level': ['advanced', 'intermediate', 'beginner'],
+    'chapter': [1, 2, 3]
+})
+
+is_less = datacamp["chapter"] < 3
+output = datacamp[is_less]
+
+print(output)
+
