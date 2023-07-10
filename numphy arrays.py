27d2192bc26b3c302a84261fc6a4a8d1d87a39d3
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-10 08:13:08 +0200

    Create numphy arrays.py

---
+++ numphy arrays.py
@@ -0,0 +1,10 @@
+#example
+
+import numpy as np
+
+numpy1 = np.array([17.2, 20.0, 8.25, 9.50])
+numpy2 = np.array([13.0, 24.0, 8.25, 9.0])
+
+output = np.logical_and(numpy1 > 10, numpy2 < 20)
+print(output)
+
